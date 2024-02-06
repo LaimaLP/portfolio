81f199858767811d8e0ae9c14551dd5f2421393c
LaimaLP <pileliene.laima@gmail.com>
2024-02-06 11:25:56 +0200

    added project to madal

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const modalOpen = document.querySelectorAll(".show-modal");
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector("[modal-close]");
 
-const modalTitle = document.getElementById('modalTitle');
-const modalImg = document.getElementById('modalImg');
-const modalImgText = document.getElementById('modalImgText');
-const modalLinkToGit = document.getElementById('modalLinkToGit');
-const modalLinkToPage = document.getElementById('modalLinkToPage');
-
+const modalTitle = document.getElementById("modalTitle");
+const modalImg = document.getElementById("modalImg");
+const modalImgText = document.getElementById("modalImgText");
+const modalLinkToGit = document.getElementById("modalLinkToGit");
+const modalLinkToPage = document.getElementById("modalLinkToPage");
 
 const projectData = [
-    {
-        title: "Game with JS",
-        img: "/img/game.png",
-        imgText: "text about JS game",
-        linkToGit: "https://github.com/LaimaLP/CatchSquare",
-        linkToPage: "https://laimalp.github.io/CatchSquare/",
-      },
-  
+  {
+    title: "Game with JS",
+    img: "/img/game.png",
+    imgText: "text about JS game",
+    linkToGit: "https://github.com/LaimaLP/CatchSquare",
+    linkToPage: "https://laimalp.github.io/CatchSquare/",
+  },
+
   {
     title: "Landing Page with JS",
     img: "/img/collab.png",
@@ -28,16 +27,32 @@ const projectData = [
     linkToGit: "https://github.com/LaimaLP/Collab-project",
     linkToPage: "https://laimalp.github.io/Collab-project/",
   },
- 
+
   {
     title: "Tic Tac Toe game",
     img: "/img/tictac.png",
     imgText: `This Tic Tac Toe game was crafted with HTML, CSS, and React. The main focus explore React's
     component-based architecture, emphasizing the using of props for data and functionality
     exchange between components.`,
-    
+
     linkToGit: "https://github.com/LaimaLP/tiktaktoe",
     linkToPage: "https://laimalp.github.io/tiktaktoe/",
+  },
+  {
+    title: "Landing Page with React",
+    img: "/img/BitMovie.png",
+    imgText: `Text about.`,
+
+    linkToGit: "https://github.com/LaimaLP/Movie-landingpage-react-main",
+    linkToPage: "https://github.com/LaimaLP/Movie-landingpage-react-main",
+  },
+  {
+    title: "Bank with PHP",
+    img: "/img/phpBank.png",
+    imgText: `Text about bank`,
+
+    linkToGit: "https://github.com/LaimaLP/bank_oop",
+    linkToPage: "https://github.com/LaimaLP/bank_oop",
   },
 ];
 
@@ -48,23 +63,35 @@ modalClose.addEventListener("click", (_) => {
 modalOpen.forEach((item) => {
   item.addEventListener("click", function () {
     if (item.getAttribute("modal-data") == "game") {
-        modalTitle.innerHTML= projectData[0].title;
-        modalImg.setAttribute('src', projectData[0].img);
-        modalImgText.innerHTML= projectData[0].imgText;
-        modalLinkToGit.setAttribute('href', projectData[0].linkToGit);
-        modalLinkToPage.setAttribute('href', projectData[0].linkToPage);
-    }else  if (item.getAttribute("modal-data") == "collab") {
-        modalTitle.innerHTML= projectData[1].title;
-        modalImg.setAttribute('src', projectData[1].img);
-        modalImgText.innerHTML= projectData[1].imgText;
-        modalLinkToGit.setAttribute('href', projectData[1].linkToGit);
-        modalLinkToPage.setAttribute('href', projectData[1].linkToPage);
-    }else  if (item.getAttribute("modal-data") == "tictac") {
-        modalTitle.innerHTML= projectData[2].title;
-        modalImg.setAttribute('src', projectData[2].img);
-        modalImgText.innerHTML= projectData[2].imgText;
-        modalLinkToGit.setAttribute('href', projectData[2].linkToGit);
-        modalLinkToPage.setAttribute('href', projectData[2].linkToPage);
+      modalTitle.innerHTML = projectData[0].title;
+      modalImg.setAttribute("src", projectData[0].img);
+      modalImgText.innerHTML = projectData[0].imgText;
+      modalLinkToGit.setAttribute("href", projectData[0].linkToGit);
+      modalLinkToPage.setAttribute("href", projectData[0].linkToPage);
+    } else if (item.getAttribute("modal-data") == "collab") {
+      modalTitle.innerHTML = projectData[1].title;
+      modalImg.setAttribute("src", projectData[1].img);
+      modalImgText.innerHTML = projectData[1].imgText;
+      modalLinkToGit.setAttribute("href", projectData[1].linkToGit);
+      modalLinkToPage.setAttribute("href", projectData[1].linkToPage);
+    } else if (item.getAttribute("modal-data") == "tictac") {
+      modalTitle.innerHTML = projectData[2].title;
+      modalImg.setAttribute("src", projectData[2].img);
+      modalImgText.innerHTML = projectData[2].imgText;
+      modalLinkToGit.setAttribute("href", projectData[2].linkToGit);
+      modalLinkToPage.setAttribute("href", projectData[2].linkToPage);
+    } else if (item.getAttribute("modal-data") == "bitMovie") {
+      modalTitle.innerHTML = projectData[3].title;
+      modalImg.setAttribute("src", projectData[3].img);
+      modalImgText.innerHTML = projectData[3].imgText;
+      modalLinkToGit.setAttribute("href", projectData[3].linkToGit);
+      modalLinkToPage.setAttribute("href", projectData[3].linkToPage);
+    } else if (item.getAttribute("modal-data") == "bank") {
+      modalTitle.innerHTML = projectData[4].title;
+      modalImg.setAttribute("src", projectData[4].img);
+      modalImgText.innerHTML = projectData[4].imgText;
+      modalLinkToGit.setAttribute("href", projectData[4].linkToGit);
+      modalLinkToPage.setAttribute("href", projectData[4].linkToPage);
     }
     modal.style.display = "block";
   });
